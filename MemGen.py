64fbda0489312ee0f6c94728ddf66ef5e0b5c941
drWoZD <drwozd@drWoZD>
2014-06-15 22:42:59 +0000

    OOP version

--- MemGen.py
+++ MemGen.py
@@ -5,60 +5,95 @@ from getopt import getopt
 import sys
 import os
 
-def print_up(draw,  size, text, color="white", fontname="ttf/DejaVuSans.ttf"):
-    n = int(size[1]/6)
+class MemGen:
+    def __init__(self, image="", uptext="", downtext="", font="", color="white"):
+        if image: self.setImage(image)
+        if uptext: self.setUpText(uptext)
+        if downtext: self.etDownText(downtext)
+        if font: self.setFont(font)
+        if color: self.setColor(color)
+        
+    def setImage(self, image):
+        self.img  = Image.open(image)
+        self.draw = ImageDraw.Draw(self.img)
+        self.size = self.img.size
 
-    font = ImageFont.truetype(fontname, n)
-    txtsz = draw.textsize(text, font)
-    while (size[0]-txtsz[0]) < 10:
-        del font
-        n = n - 1
-        font = ImageFont.truetype(fontname, n)
-        txtsz = draw.textsize(text, font)
+    def setUpText(self, text):
+        self.uptext = text
+        
+    def setDownText(self, text):
+        self.downtext = text
+        
+    def setFont(self, name):
+        self.fontname = name
+        
+    def setColor(self, name):
+        self.color = name
+        
+    def printUp(self):
+        n = int(self.size[1]/6)
 
-    textpos = (int((size[0] - txtsz[0])/2), 0)
-    draw.text(textpos, text, fill=color, font=font)
+        font = ImageFont.truetype(self.fontname, n)
+        txtsz = self.draw.textsize(self.uptext, font)
+        while (self.size[0]-txtsz[0]) < 10:
+           del font
+           n = n - 1
+           font = ImageFont.truetype(self.fontname, n)
+           txtsz = self.draw.textsize(self.uptext, font)
 
-def print_down(draw,  size, text, color="white", fontname="ttf/DejaVuSans.ttf"):
-    n = int(size[1]/6)
+        textpos = (int((self.size[0] - txtsz[0])/2), 0)
+        self.draw.text(textpos, self.uptext, fill=self.color, font=font)
+    
+    def printDown(self):
+        n = int(self.size[1]/6)
 
-    font = ImageFont.truetype(fontname, n)
-    txtsz = draw.textsize(text, font)
-    while (size[0]-txtsz[0]) < 10:
-        del font
-        n = n - 1
-        font = ImageFont.truetype(fontname, n)
-        txtsz = draw.textsize(text, font)
+        font = ImageFont.truetype(self.fontname, n)
+        txtsz = self.draw.textsize(self.downtext, font)
+        while (self.size[0]-txtsz[0]) < 10:
+            del font
+            n = n - 1
+            font = ImageFont.truetype(self.fontname, n)
+            txtsz = self.draw.textsize(self.downtext, font)
 
-    textpos = (int((size[0] - txtsz[0])/2), size[1]-txtsz[1])
-    draw.text(textpos, text, fill=color, font=font)
-
-
-
+        textpos = (int((self.size[0] - txtsz[0])/2), self.size[1]-txtsz[1])
+        self.draw.text(textpos, self.downtext, fill=self.color, font=font)
+    
+    def generate(self):
+        if self.uptext:
+	    self.printUp()
+	if self.downtext:
+	    self.printDown()
+	    
+    def save(self, path="out.png"):
+        self.img.save(path, "PNG")
+        
 if __name__ == "__main__":
-    out = "out.png"
-    color = "green"
-    font="ttf/DejaVuSans.ttf"
+    #out = "out.png"
+    #color = "green"
+    #font = os.path.realpath(__file__)) + 
     opts, args = getopt(sys.argv[1:], "u:d:s:o:c:f:", ["uptext=", "downtext=", "source-image=", "output-image=", "color=", "font="])
+    mem = MemGen(font=os.path.dirname(os.path.realpath(__file__)) + "/ttf/DejaVuSans.ttf")
     for o, a in opts:
         if o in ("-u", "--uptext"):
-            uptext = unicode(a, 'utf-8')
+            mem.setUpText(unicode(a, 'utf-8'))
         elif o in ("-d", "--downtext"):
-            downtext = unicode(a, 'utf-8')
+            mem.setDownText(unicode(a, 'utf-8'))
         elif o in ("-s", "--source-image"):
             file = os.path.dirname(os.path.realpath(__file__)) + "/images/" + a
             file = [a, file][os.path.exists(file)]
-            img = Image.open(file)
-            draw = ImageDraw.Draw(img)
+            mem.setImage(file)
         elif o in ("-o", "--output-image"):
             out = a
         elif o in ("-c", "--color"):
-            color = a
+            mem.setColor(a)
         elif o in ("-f", "--font"):
             file = os.path.dirname(os.path.realpath(__file__)) + "/ttf/" + a
             file = [a, file][os.path.exists(file)]
-            font = file
+            mem.setFont(file)
 
+    mem.generate()
+    mem.save();
+    """ 
     while 1:
         try:
             print_up(draw, img.size, uptext, color, font)
@@ -74,4 +109,4 @@ if __name__ == "__main__":
                 uptext = unicode(raw_input("Enter up text: "), 'utf-8')
             elif t == "downtext":
                 downtext = unicode(raw_input("Enter down text: "), 'utf-8')
-
+    """
